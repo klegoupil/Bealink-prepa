18ba660dd28614ae1078a517f2190cc7c9842cbc
Kévin <kevin.legoupil@laposte.net>
2024-02-15 22:55:14 +0100

    Init Console App 🔨 🔧 🙈  to call Web Api Rest

---
+++ WebApiRest/ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+try
+{
+    HttpClient httpClient = new()
+    {
+        BaseAddress = new Uri("http://localhost:50798/"),
+    };
+
+    using HttpResponseMessage response = await httpClient.GetAsync("WeatherForecast");
+
+    var jsonResponse = await response.Content.ReadAsStringAsync();
+    Console.WriteLine($"{jsonResponse}\n");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Exception message : {ex.Message}");
+}
+finally
+{
+    Console.ReadKey(); ;
+}
